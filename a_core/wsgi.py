1b2e7bb1e7e80eb18062442091724bb3802d7db6
kodoshin <kodoshin4@gmail.com>
2025-04-05 19:33:23 -0400

    WSGI changes for deployment

--- a_core/wsgi.py
+++ a_core/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'a_core.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'a_core.settings'
+#settings_module = 'a_core.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'a_core.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
 
 application = get_wsgi_application()
