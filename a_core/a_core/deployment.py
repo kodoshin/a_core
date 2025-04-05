f75f038a447874d86e3efd864ceba4b27a144025
kodoshin <kodoshin4@gmail.com>
2025-04-05 01:17:50 -0400

    Deployment changes

---
+++ a_core/a_core/deployment.py
@@ -0,0 +1,76 @@
+import os
+from .settings import *
+from .settings import BASE_DIR
+
+
+SECRET_KEY = os.environ['SECRET']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+DEBUG = False
+
+# WhiteNoise configuration
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
+    'a_users.middleware.ProfileCompletionMiddleware',
+]
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
+env = environ.Env()
+env_path = os.path.join(BASE_DIR, ".env")  # BASE_DIR est défini en haut de settings.py
+if os.path.exists(env_path):
+    environ.Env.read_env(env_path)
+    #print("Fichier .env chargé avec succès")
+else:
+    print("Le fichier .env est manquant !")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
+
+
+#conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+#conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': conn_str_params['dbname'],
+#        'HOST': conn_str_params['host'],
+#        'USER': conn_str_params['user'],
+#        'PASSWORD': conn_str_params['password'],
+#    }
+#}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- a_core/a_core/wsgi.py
+++ a_core/a_core/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
+settings_module = 'a_core.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'a_core.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
