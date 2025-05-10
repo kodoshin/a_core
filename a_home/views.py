3603451d2d17d903fb84bc96f452afc114cbc42c
kodoshin <kodoshin4@gmail.com>
2025-05-09 20:53:16 -0400

    Integrate native tech stack gallery in home.html

--- a_home/views.py
+++ a_home/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Persona
-from a_projects.models import Project, Status
+from a_projects.models import Project, Status, Technology
 from .ai_tools_radar_chart import create_ai_tools_radar_chart
 
 
 def home(request):
     personas = Persona.objects.all()
     radar_chart = create_ai_tools_radar_chart()
+    technologies = Technology.objects.exclude(name='Other')
     try:
         status = Status.objects.get(code=1)
     except :
@@ -14,9 +15,9 @@ def home(request):
     try:
         user = request.user
         user_projects = Project.objects.filter(user=user, status=status)
-        context = {'personas': personas, 'projects': user_projects}
+        context = {'personas': personas, 'projects': user_projects, 'technologies': technologies}
     except:
-        context = {'personas': personas, 'radar_chart': radar_chart }
+        context = {'personas': personas, 'radar_chart': radar_chart, 'technologies': technologies}
 
 
     return render(request, 'home.html', context)
