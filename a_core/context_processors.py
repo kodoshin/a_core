aebc5e974d854ea8f318ce0eb2dad7f1d6ab7c2b
kodoshin <kodoshin4@gmail.com>
2025-05-17 01:24:45 -0400

    Right/left arrows related to clicked chat message

--- a_core/context_processors.py
+++ a_core/context_processors.py
@@ -1,3 +1,5 @@
+
+
 import pytz
 from django.utils import timezone
 
@@ -17,3 +19,26 @@ def reset_daily_credits(request):
             profile.has_claimed_credits = False
             profile.save()
     return {}
+
+"""
+
+import pytz
+from django.utils import timezone
+from asgiref.sync import sync_to_async
+
+async def reset_daily_credits(request):
+    if not request.user.is_superuser and request.user.is_authenticated:
+        # Load the profile asynchronously to avoid sync DB access in async context
+        profile = await sync_to_async(lambda: request.user.profile)()
+        try:
+            user_tz = pytz.timezone(profile.timezone) if profile.timezone else timezone.get_current_timezone()
+        except Exception:
+            user_tz = timezone.get_current_timezone()
+        local_today = timezone.now().astimezone(user_tz).date()
+        if profile.daily_credit_claim_date != local_today and profile.has_claimed_credits:
+            profile.has_claimed_credits = False
+            # Save the profile asynchronously
+            await sync_to_async(profile.save)()
+    return {}
+    
+"""
